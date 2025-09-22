a526f142dfdbf3aab5310f41ce35b1141a7a384a
omdien <87163559+omdien@users.noreply.github.com>
2025-09-22 10:25:38 +0700

    update coonfig database untuk membedakan host export dan siap mutu

--- config/Database.js
+++ config/Database.js
@@ -8,19 +8,19 @@ export const db_ekspor = new Sequelize(process.env.DB_NAME, process.env.DB_USER,
     timezone: '+07:00'
 });
 
-export const db_hc = new Sequelize(process.env.DB_NAME_HC, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+export const db_hc = new Sequelize(process.env.DB_NAME_HC, process.env.DB_USER_SIAP_MUTU, process.env.DB_PASS_SIAP_MUTU, {
+    host: process.env.DB_HOST_SIAP_MUTU,
     dialect: 'mysql'
 });
 
-export const db_mutu = new Sequelize(process.env.DB_NAME_MUTU, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+export const db_mutu = new Sequelize(process.env.DB_NAME_MUTU, process.env.DB_USER_SIAP_MUTU, process.env.DB_PASS_SIAP_MUTU, {
+    host: process.env.DB_HOST_SIAP_MUTU,
     dialect: 'mysql',
     timezone: '+07:00'
 });
 
-export const db_pnbp = new Sequelize(process.env.DB_NAME_PNBP, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+export const db_pnbp = new Sequelize(process.env.DB_NAME_PNBP, process.env.DB_USER_SIAP_MUTU, process.env.DB_PASS_SIAP_MUTU, {
+    host: process.env.DB_HOST_SIAP_MUTU,
     dialect: 'mysql',
     timezone: '+07:00'
 });
